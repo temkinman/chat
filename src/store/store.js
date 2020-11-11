230f70d71895602d3f56702e42559b6b7a29fcfc
temkinman <temkinman@mail.ru>
2020-11-11 15:32:18 +0300

    update store

--- src/store/store.js
+++ src/store/store.js
@@ -5,47 +5,6 @@ export const initialState = {
     avatar: "img",
   },
     chats:{},
-  // chats: {
-  //   111: {
-  //     title: "onliner",
-  //     id: "111",
-  //     messages: [
-  //       {
-  //         time: "2020-07-20T14:12",
-  //         from: "onliner",
-  //         text: "Hello Belarus",
-  //         messageId: "-1",
-  //       },
-  //     ],
-  //     draft: "",
-  //   },
-  //   777: {
-  //     title: "nexta",
-  //     id: "777",
-  //     messages: [
-  //       {
-  //         time: "2020-09-30T20:00",
-  //         from: "nexta",
-  //         text: "Hello",
-  //         messageId: "-2",
-  //       },
-  //     ],
-  //     draft: "",
-  //   },
-  //   888: {
-  //     title: "tutby",
-  //     id: "888",
-  //     messages: [
-  //       {
-  //         time: "2020-08-30T24:00",
-  //         from: "tutBY",
-  //         text: "Hi",
-  //         messageId: "-3",
-  //       },
-  //     ],
-  //     draft: "",
-  //   },
-  // },
   currentChatId: null,
   newChatModal: false,
   contextMenu: false,
